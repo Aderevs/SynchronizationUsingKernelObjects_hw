1e8379f2591d433f8dc477cd9af554945d5a4d22
YuriiSvereda <svereda.yurii@chnu.edu.ua>
2024-02-19 14:43:15 +0200

    working version of task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static Mutex mutex = new Mutex(false, "MyMutex");
+        static void Main(string[] args)
+        {
+            mutex.WaitOne();
+
+            for(int i = 0; i < 20; i++) 
+            {
+                Console.WriteLine("{0}. Hello, World!", i);
+                Thread.Sleep(500);
+            }
+            mutex.ReleaseMutex();
+        }
+    }
+}
